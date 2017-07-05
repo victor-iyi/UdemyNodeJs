81119bc2cad9f2e0ab723052b464da6efa356692
victor-iyiola <javafolabi@gmail.com>
2017-07-05 21:56:24 +0100

    first class function and function expression

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!')
+// function statement
+function greet() {
+	console.log('hi')
+}
+greet()
+
+// functions are first-class
+function logGreeting(fn) {
+	fn()
+}
+logGreeting(greet)
+
+// function expression
+const greetMe = function() {
+	console.log('Hello')
+}
+
+logGreeting(greetMe)

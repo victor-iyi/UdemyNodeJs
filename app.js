a8069fa605faeadb59968bdd0fe8b5a9d522281b
victor-iyiola <javafolabi@gmail.com>
2017-07-13 02:56:41 +0100

    Inheriting from EventEmitter

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+const EventEmitter = require('events')
+const util = require('util')
+
+function Greetr() {
+	this.greeting = 'Hello, World!'
+}
+
+util.inherits(Greetr, EventEmitter)
+
+Greetr.prototype.greet = function () {
+	console.log(this.greeting)
+	this.emit('greet')
+}
+
+const grtr = new Greetr()
+grtr.on('greet', function() {
+	console.log('Greetings to you too')
+})
+grtr.greet()

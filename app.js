110565b01cb438bdab2db97a2df4ccaebe1cee58
victor-iyiola <javafolabi@gmail.com>
2017-07-15 07:27:49 +0100

    Templates and serving HTML files

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const http = require('http');
+const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end('<h1>Hello, World!</h1>');
-});
-server.listen(4000, 'localhost');
-console.log('listening on http://localhost:4000')
+const PORT = process.env.PORT || 4000
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html' })
+	let doc = fs.readFile(`${ __dirname }/index.html`, 'utf8', (err, data) => {
+		data = data.replace('{name}', 'Victor I. Afolabi')
+		data = data.replace('{age}', 21)
+		res.end(data)
+	})
+})
+server.listen(PORT, 'localhost')
+console.log(`Server listening @ http://localhost:${ PORT }`)
